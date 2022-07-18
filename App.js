65fd9651b4e8b27313ea21327a74c7dc0ede7244
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2022-07-18 05:13:20 -0700

    starter

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+
+export default function App() {
+  return (
+   <Home/>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
